8a6638097a1d81104d6e7efe8bfbfaa3f10c16cc
bryan <bca6@gmail.com>
2015-12-08 01:49:35 -0600

    added some shiny stuff

---
+++ 03 Shiny/server.R
@@ -0,0 +1,132 @@
+# server.R
+require(jsonlite)
+require(RCurl)
+require(ggplot2)
+require(dplyr)
+require(tidyr)
+require(shiny)
+require(leaflet)
+require(shinydashboard)
+require(DT)
+
+shinyServer(function(input, output) {
+  
+  df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from GENERAL_STATS order by pos"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_ba7433', PASS='orcl_ba7433', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE) ))
+  
+  
+  
+  
+  
+  
+  #Points By Position Bar Chart
+  output$pointsbyteam <- renderPlot({
+    NBA <- df%>%filter(TM != "TOT", TM != "Tm")
+    plot2 <- ggplot(NBA, aes(TM, PTS, fill=TM)) + geom_bar(stat="identity") + coord_flip()
+    
+    return(plot2)
+    
+  })
+  
+  
+  #Points By Division Bar Chart
+  output$pointsbypos <- renderPlot({
+    NBA <- df%>%filter(TM != "TOT", TM != "Tm")
+    plot3 <- ggplot(NBA, aes(TM, PTS, fill=POS)) + geom_bar(stat="identity") + coord_flip()
+    
+    return(plot3)
+  })
+  
+  
+  output$assistsbypos <- renderPlot({
+    NBA <- df%>%filter(TM != "TOT", TM != "Tm")
+    plot4 <- ggplot() + 
+      coord_cartesian() + 
+      scale_x_continuous() +
+      scale_y_continuous() +
+      labs(title='Assists By Position') +
+      labs(x="Minutes Played", y=paste("Assists")) +
+      layer(data=NBA, 
+            mapping=aes(x=as.numeric(as.character(MP)), y=as.numeric(as.character(AST)), color=POS), 
+            stat="identity", 
+            stat_params=list(), 
+            geom="point",
+            geom_params=list(), 
+            #position=position_identity()
+            position=position_jitter(width=0.3, height=0)
+      )
+    
+    return(plot4)
+    
+  })
+  
+  output$rebsbypos <- renderPlot({
+    NBA <- df%>%filter(TM != "TOT", TM != "Tm")
+    plot5 <- ggplot() + 
+      coord_cartesian() + 
+      scale_x_continuous() +
+      scale_y_continuous() +
+      labs(title='Rebounds By Position') +
+      labs(x="Minutes Played", y=paste("Total Rebounds")) +
+      layer(data=NBA, 
+            mapping=aes(x=as.numeric(as.character(MP)), y=as.numeric(as.character(TRB)), color=POS), 
+            stat="identity", 
+            stat_params=list(), 
+            geom="point",
+            geom_params=list(), 
+            #position=position_identity()
+            position=position_jitter(width=0.3, height=0)
+      )
+    
+    return(plot5)
+    
+  })
+  
+  output$stealsbypos <- renderPlot({
+    NBA <- df%>%filter(TM != "TOT", TM != "Tm")
+    plot6 <- ggplot() + 
+      coord_cartesian() + 
+      scale_x_continuous() +
+      scale_y_continuous() +
+      labs(title='Steals By Position') +
+      labs(x="Personal Fouls", y=paste("Steals")) +
+      layer(data=NBA, 
+            mapping=aes(x=as.numeric(as.character(PF)), y=as.numeric(as.character(STL)), color=POS), 
+            stat="identity", 
+            stat_params=list(), 
+            geom="point",
+            geom_params=list(), 
+            #position=position_identity()
+            position=position_jitter(width=0.3, height=0)
+      )
+    
+    return(plot6)
+    
+  })
+  
+  output$blocksbypos <- renderPlot({
+    NBA <- df%>%filter(TM != "TOT", TM != "Tm")
+    plot7 <- ggplot() + 
+      coord_cartesian() + 
+      scale_x_continuous() +
+      scale_y_continuous() +
+      labs(title='Blocks By Position') +
+      labs(x="Personal Fouls", y=paste("Blocks")) +
+      layer(data=NBA, 
+            mapping=aes(x=as.numeric(as.character(PF)), y=as.numeric(as.character(BLK)), color=POS), 
+            stat="identity", 
+            stat_params=list(), 
+            geom="point",
+            geom_params=list(), 
+            #position=position_identity()
+            position=position_jitter(width=0.3, height=0)
+      ) 
+    
+    return(plot7)
+    
+  })
+  
+  
+  
+  
+  
+})
---
+++ 03 Shiny/ui.R
@@ -0,0 +1,61 @@
+#ui.R 
+
+library(shiny)
+require(shinydashboard)
+require(leaflet)
+
+# Define UI for application that plots random distributions 
+dashboardPage(
+  # Application title
+  dashboardHeader(title = "2014-2015 NBA Basketball Data", titleWidth = 350),
+  dashboardSidebar(width = 350,
+                   sidebarMenu(
+                     menuItem("Points By Team", tabName = "pointsbyteam", icon = icon("bar-chart")),
+                     menuItem("Points By Position", tabName = "pointsbypos", icon = icon("bar-chart")),
+                     menuItem("Assists By Position", tabName = "assistsbypos", icon = icon("line-chart")),
+                     menuItem("Rebounds By Position", tabName = "rebsbypos", icon = icon("line-chart")),
+                     menuItem("Steals By Position", tabName = "stealsbypos", icon = icon("line-chart")),
+                     menuItem("Blocks By Position", tabName = "blocksbypos", icon = icon("line-chart"))
+                   )
+  ),
+  dashboardBody(
+    tabItems(
+      #first tab content
+      tabItem(tabName = "pointsbyteam",
+              h4("Points By Team"),
+              plotOutput("pointsbyteam")
+        
+      ),
+      
+      tabItem(tabName = "pointsbypos",
+              h4("Points By Position"),
+              plotOutput("pointsbypos")
+      ),
+      
+      tabItem(tabName = "assistsbypos",
+              h4("Assists By Position"),
+              plotOutput("assistsbypos")
+      ),
+      
+      tabItem(tabName = "rebsbypos",
+              h4("Rebounds By Position"),
+              plotOutput("rebsbypos")
+      ),
+      
+      tabItem(tabName = "stealsbypos",
+              h4("Steals By Position"),
+              plotOutput("stealsbypos")
+      ),
+      
+      tabItem(tabName = "blocksbypos",
+              h4("Blocks By Position"),
+              plotOutput("blocksbypos")
+      )
+      
+    )  
+  
+  
+  )
+  
+  
+)
